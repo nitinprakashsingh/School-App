7f6ac8ee6b93fcde5b5f64651698c1b1b6a08d56
SINGH Nitin <nitin.singh2@soprasteria.com>
2025-08-16 09:43:36 +0530

    first apk generated and found safe ar</SafeAreaView>ea view issue fixes

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 
 import React from 'react';
 import { AppNavigator } from './src/Navigation';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 function App(): React.JSX.Element {
-  return <AppNavigator />;
+  return(
+  <SafeAreaView style={{ flex: 1, backgroundColor: 'red' }}>
+   <AppNavigator />;
+   </SafeAreaView>
+  )
 }
 
 export default App;
